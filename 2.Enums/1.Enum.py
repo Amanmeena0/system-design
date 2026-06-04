371a1916c8aa890f6e5f863116677377fab67a17
Amanmeena0 <meenaaman581@gmail.com>
2026-06-04 08:23:21 +0530

    Refactor Order class to OrderStatus enum and add Coin enum with value retrieval

--- 2.Enums/1.Enum.py
+++ 2.Enums/1.Enum.py
@@ -1,6 +1,34 @@
 from enum import Enum
 
-class Order:
+class OrderStatus(Enum):
 
-    def __init__(self):
-        self.order_id = 
+    PLACED = "PLACED"
+    CONFIRMED = "CONFIRMED"
+    SHIPPED = "SHIPPED"
+    DELIVERED = "DELIVERED"
+    CANCELLED = "CANCELLED"
+
+
+status = OrderStatus.SHIPPED
+
+if status == OrderStatus.SHIPPED:
+    print("Your package is on the way")
+
+class Coin(Enum):
+
+    PENNY = 1
+    NICKEL = 5
+    DIME = 10
+    QUARTER = 25
+
+    def __init__(self, value):
+        self.coin_value = value
+    
+    def get_value(self):
+        return self.coin_value
+    
+
+
+total = Coin.DIME.get_value() + Coin.QUARTER.get_value()
+
+print(total)
