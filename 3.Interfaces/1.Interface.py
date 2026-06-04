217dc1319975fa242b8bee4dd6ff931804492f45
Amanmeena0 <meenaaman581@gmail.com>
2026-06-04 10:03:41 +0530

    Add CheckoutService class to manage payment gateway and checkout process

--- 3.Interfaces/1.Interface.py
+++ 3.Interfaces/1.Interface.py
@@ -13,4 +13,12 @@ class RajorpayPayment(PaymentGateway):
     def initiate_payment(self, amount):
         print(f"Processing payment via Razorpay: Rupees{amount}")
 
-        
+class CheckoutService:
+    def __init__(self,payment_gateway):
+        self.payment_gateway = payment_gateway
+
+    def set_payment_gateway(self, payment_gateway):
+        self.payment_gateway = payment_gateway
+
+    def checkout(self, amount):
+        self.payment_gateway.initiate_payment(amount)
